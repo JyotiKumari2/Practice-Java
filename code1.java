5a576ba98e05c82b786441ede181b2185ba8d5e8
Jyoti Kumari <jyotikm1801@gmail.com>
2023-01-28 01:36:27 +0530

    28Jan

---
+++ code1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class code1 {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  int count = 0;
+
+   int no = sc.nextInt();
+while(no>0){
+int remainder = no%10;
+if(remainder%2==0){
+count++;
+}
+no = no/10;
+}
+if(count>=3){
+System.out.println("TRUE");
+}else{
+System.out.println("FALSE");
+}
+}
+}
+
+ 
